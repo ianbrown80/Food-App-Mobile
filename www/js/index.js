d9602d08e1ff9d4dede6578bd9ec16e5d0215a07
Ian Brown <brown.i18@sky.com>
2020-06-03 20:54:47 +0100

    Created login and register pages

--- www/js/index.js
+++ www/js/index.js
@@ -1,46 +1,70 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+  // Application Constructor.
+  initialize: function() {
+      document.addEventListener( 'deviceready', this.onDeviceReady.bind( this ), false );
+  },
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+  // deviceready Event Handler.
+  onDeviceReady: function() {
+      this.receivedEvent('deviceready');
+  },
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+  // Update DOM on a Received Event.
+  receivedEvent: function(id) {
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+    var self = this;
 
-        console.log('Received Event: ' + id);
-    }
+    console.log('Here');
+
+    $( '#register-form' ).on( 'submit', function( event ) {
+
+        event.preventDefault();
+
+        var data = {
+            name : $( '#register-name' ).val(),
+            email : $( '#register-email' ).val(),
+            password : $( '#register-password' ).val()
+        }
+
+        $.post( 'http://localhost:8080/foodApp/webresources/users', function( response, status, xhr ) {
+          console.log( response );
+          console.log( status );
+          console.log( xhr );
+        })
+
+        var url = 'http://localhost:8080/foodApp/webresources/users';
+        self.getJSON( url,
+            function(err, data) {
+                if (err !== null) {
+                  alert('Something went wrong: ' + err);
+                } else {
+                  console.log(data);
+                }
+            }
+            
+        );
+
+    });
+
+  },
+
+  getJSON: function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+},
 };
+
+//http://localhost:8080/foodApp/webapi/users
 
 app.initialize();
